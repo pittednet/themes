d45fbd91d334b6a6e189ee9488e6aebb5d906dab
pittednet <26190410+pittednet@users.noreply.github.com>
2019-05-27 15:04:47 +0100

    Update archive desc

--- intergalactic-2/archive-recipes.php
+++ intergalactic-2/archive-recipes.php
@@ -19,6 +19,7 @@ get_header(); ?>
 			<header class="page-header">
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					get_the_post_type_description();
 					the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
